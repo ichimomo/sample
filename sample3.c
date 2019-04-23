22f4fa750786b8ba02aeeb0fb540e84dcca5ff66
Shin FUKUI <fshin3@nadesico.local>
2019-04-23 11:30:08 +0900

    <added sum2 calculation.>

--- sample3.c
+++ sample3.c
@@ -3,11 +3,13 @@
 int main(void){
 	int i;
 	int j;
+	int k;
 	for(i=0; i<10; i++){
 		j = i++;
 	}
 	printf("sum is %d.\n",j);
-	
+	k = j;
+	print("sum2 = %d.\n",k);
 	printf("Hello, FRA!\n");
 	return 0;
 }
